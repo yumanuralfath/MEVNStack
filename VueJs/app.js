c456831c50838bcf5bce62e1ed8b1db947245dbb
Yumana <yumanuralfath2@gmail.com>
2024-01-10 10:10:24 +0700

    BeljarVueJS2

---
+++ VueJs/app.js
@@ -0,0 +1,73 @@
+var dataProduct = {
+  maximum: 500,
+  products: [
+    {
+      name: 'Bamboo Thermal ski coat',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim alias, dicta placeat dignissimos autem rerum cupiditate maxime, unde voluptates nemo magni libero eum reiciendis, eligendi suscipit ullam quam minima incidunt!',
+      price: '10',
+      image: 'https://source.unsplash.com/360x200?ski+coat',
+    },
+    {
+      name: 'Mechanical Keyboard',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim alias, dicta placeat dignissimos autem rerum cupiditate maxime, unde voluptates nemo magni libero eum reiciendis, eligendi suscipit ullam quam minima incidunt!',
+      price: '300',
+      image: 'https://source.unsplash.com/360x200?mechanical+Keyboard',
+    },
+    {
+      name: 'Action Figure Anime',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim alias, dicta placeat dignissimos autem rerum cupiditate maxime, unde voluptates nemo magni libero eum reiciendis, eligendi suscipit ullam quam minima incidunt!',
+      price: '400',
+      image: 'https://source.unsplash.com/360x200?Action+Figure',
+    },
+    {
+      name: 'Anime',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim alias, dicta placeat dignissimos autem rerum cupiditate maxime, unde voluptates nemo magni libero eum reiciendis, eligendi suscipit ullam quam minima incidunt!',
+      price: '220',
+      image: 'https://source.unsplash.com/360x200?Anime',
+    },
+    {
+      name: 'Tank',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim alias, dicta placeat dignissimos autem rerum cupiditate maxime, unde voluptates nemo magni libero eum reiciendis, eligendi suscipit ullam quam minima incidunt!',
+      price: '1000',
+      image: 'https://source.unsplash.com/360x200?tank',
+    },
+    {
+      name: 'Orange Juice',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim alias, dicta placeat dignissimos autem rerum cupiditate maxime, unde voluptates nemo magni libero eum reiciendis, eligendi suscipit ullam quam minima incidunt!',
+      price: '40',
+      image: 'https://source.unsplash.com/360x200?Juice',
+    },
+    {
+      name: 'Bakso',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim alias, dicta placeat dignissimos autem rerum cupiditate maxime, unde voluptates nemo magni libero eum reiciendis, eligendi suscipit ullam quam minima incidunt!',
+      price: '23',
+      image: 'https://source.unsplash.com/360x200?meatball',
+    },
+    {
+      name: 'Laptop',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim alias, dicta placeat dignissimos autem rerum cupiditate maxime, unde voluptates nemo magni libero eum reiciendis, eligendi suscipit ullam quam minima incidunt!',
+      price: '400',
+      image: 'https://source.unsplash.com/360x200?laptop',
+    },
+    {
+      name: 'Server',
+      description:
+        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim alias, dicta placeat dignissimos autem rerum cupiditate maxime, unde voluptates nemo magni libero eum reiciendis, eligendi suscipit ullam quam minima incidunt!',
+      price: '500',
+      image: 'https://source.unsplash.com/360x200?server',
+    },
+  ],
+};
+
+var app = new Vue({
+  el: '#app',
+  data: dataProduct,
+});
